9dff62a65d383ef8710507c45c97e60a52adfc1a
DanielKinnito <mokonnendaniel@gmail.com>
2024-07-30 10:54:29 +0300

    :zap: Completed root.dart.

--- mobile_Daniel_Mekonnen/task_6/lib/main.dart
+++ mobile_Daniel_Mekonnen/task_6/lib/main.dart
@@ -1,5 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:task_6/home_page.dart';
 
 void main() {
   runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.white as MaterialColor,
+      ),
+    );
+  }
+}
+
+class RootPageState extends StatefulWidget {
+  const RootPageState({super.key});
+
+  @override
+  State<RootPageState> createState() => _RootPageStateState();
+}
+
+class _RootPageStateState extends State<RootPageState> {
+  int currentPage = 0;
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body:  HomePage(),      
+    );
+  }
 }
