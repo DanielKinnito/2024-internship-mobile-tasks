6a6d026a797d07cba4ea0177a83269fc23118a49
DanielKinnito <mokonnendaniel@gmail.com>
2024-08-05 15:18:02 +0300

    :arrow_up: Upgrade dependencies

--- ecommerce_app/lib/main.dart
+++ ecommerce_app/lib/main.dart
@@ -1,3 +1,8 @@
+import 'package:ecommerce_app/presentation/pages/add_update_page.dart';
+import 'package:ecommerce_app/presentation/pages/details_page.dart';
+import 'package:ecommerce_app/presentation/pages/home_page.dart';
+import 'package:ecommerce_app/presentation/pages/search_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,6 +13,50 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        textTheme: GoogleFonts.poppinsTextTheme(),
+      ),
+      home: const RootPage(),
+      onGenerateRoute: (settings) {
+        switch (settings.name) {
+          case '/details':
+            return MaterialPageRoute(
+              builder: (context) => const DetailsPage(),
+            );
+          case '/add_update':
+            return MaterialPageRoute(
+              builder: (context) => const AddUpdatePage(),
+            );
+          case '/search':
+            return MaterialPageRoute(
+              builder: (context) => const SearchPage(),
+            );
+          default:
+            return MaterialPageRoute(
+              builder: (context) => const RootPage(),
+            );
+        }
+      },
+    );
+  }
+}
+
+class RootPage extends StatefulWidget {
+  const RootPage({super.key});
+
+  @override
+  State<RootPage> createState() => _RootPageState();
+}
+
+class _RootPageState extends State<RootPage> {
+  int currentIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: HomePage(),
+    );
   }
 }
